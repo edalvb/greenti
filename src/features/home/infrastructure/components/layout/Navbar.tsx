28d793a781384126de642e16247b95af2fb6a733
Edward <edalvb@gmail.com>
2025-05-20 19:16:18 -0500

    main: add px-responsive to navbar

--- src/features/home/infrastructure/components/layout/Navbar.tsx
+++ src/features/home/infrastructure/components/layout/Navbar.tsx
@@ -65,7 +65,7 @@ export const Navbar: React.FC = () => {
 
   return (
     <nav
-      className={`fixed w-full z-50 top-0 transition-all duration-300 ${isScrolled ? "bg-primary shadow-lg py-2" : "bg-transparent py-4"}`}
+      className={`fixed w-full z-50 top-0 transition-all duration-300 px-responsive ${isScrolled ? "bg-primary shadow-lg py-2" : "bg-transparent py-4"}`}
     >
       <div className="container mx-auto px-4 sm:px-6 lg:px-8">
         <div className="flex items-center justify-between h-16">
