1c66a77b91751532e1bf5f82312bf452f393ff29
Edward <edalvb@gmail.com>
2025-09-03 16:30:36 -0300

    feat: eliminar sección ContactSection de la página de aterrizaje

--- src/app/[locale]/(landing)/page.tsx
+++ src/app/[locale]/(landing)/page.tsx
@@ -44,7 +44,6 @@ export default async function LandingPage({ params }: any) {
       <ClientLogosSection />
       <CtaBannerSection />
       <FaqSection />
-      <ContactSection />
     </>
   );
 }
