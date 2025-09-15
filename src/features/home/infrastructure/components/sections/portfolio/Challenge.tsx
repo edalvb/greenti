426065f17c0c34cb3cbf68091edc67607d699909
Edward Alexis <edalvb@gmail.com>
2025-09-15 19:47:35 -0300

    feat: agregar margen izquierdo a los encabezados y párrafos en Challenge para mejorar la presentación

--- src/features/home/infrastructure/components/sections/portfolio/Challenge.tsx
+++ src/features/home/infrastructure/components/sections/portfolio/Challenge.tsx
@@ -3,11 +3,11 @@ export const Challenge = ({ description }: { description: string }) => {
     <div className="relative z-[2] mt-16 md:mt-24 grid grid-cols-1 md:grid-cols-2 gap-10 md:gap-16 ml-10">
       <section id="challenge">
         <div>
-          <h3 className="text-3xl md:text-5xl font-bold mb-4">
+          <h3 className="text-3xl md:text-5xl font-bold mb-4 ml-2">
             <span className="text-secondary">El </span>
             <span className="text-primary">desafío</span>
           </h3>
-          <p className="text-secondary leading-relaxed max-w-[544px]">
+          <p className="text-secondary leading-relaxed max-w-[544px] pl-2">
             {description}
           </p>
         </div>
